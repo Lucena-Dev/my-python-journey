f54739914337746d1f225464ff6a150f33493f26
Isaac Lucena Neri <Sandylucena2008@gmail.com>
2025-08-02 18:25:54 -0300

    Create exercicio1.py

---
+++ operacoes-aritmeticas-matematicas/exercicio1.py
@@ -0,0 +1,16 @@
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outri número inteiro: '))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+divisao_inteira = n1 // n2
+resto = n1 % n2
+
+print(f'A soma dos dois númeoros é {soma}')
+print(f'A subtração dos dois números é {subtracao}')
+print(f'A multiplicação dos dois números é {multiplicacao}')
+print(f'A divisão dos dois números é {divisao}')
+print(f'A divisão inteira dos dois números é {divisao_inteira}')
+print(f'O resto da divisão é {resto}')
