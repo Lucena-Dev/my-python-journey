9d0a306969028d3775869e776e319e7022d57a10
Isaac Lucena Neri <Sandylucena2008@gmail.com>
2025-08-03 18:10:54 -0300

    Create exercicio3.py
    Peça um número ao usuário e mostre a raiz quadrada e o quadrado desse número

---
+++ modulos/exercicio3.py
@@ -0,0 +1,9 @@
+import math
+
+numero = int(input('Digite um número: '))
+
+raiz = math.sqrt(numero)
+quadrado = numero * numero
+
+print(f'A raiz quadrada do número é {raiz:.2f}')
+print(f'O quadrado do número é {quadrado}')
